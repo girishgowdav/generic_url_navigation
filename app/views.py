aa23d815e1400d96105ba4146fe976d2d8d6a37d
girishgowdav <gowdagirishv38@gmail.com>
2023-06-01 13:48:43 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def giri(request):
+    return render(request,'giri.html')
+
+
+def srujana(request):
+    return render(request,'srujana.html')
